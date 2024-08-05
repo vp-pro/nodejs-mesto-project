329c0fcd6b38ee112b7a3f3e5798132a62c05dc2
Vladislav Panferov <panfyor26@gmail.com>
2024-08-05 18:18:04 +1100

    upt

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,15 @@
 import express, { Response, Request, NextFunction } from 'express';
 import mongoose from 'mongoose';
 import helmet from 'helmet';
-
 import router from './routes';
-
 import errorHandler from './middlewares/error-handler';
 
 const { PORT = 3000 } = process.env;
-
 const app = express();
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use(helmet());
-
 app.use(express.json());
 
 app.use((req: Request, res: Response, next: NextFunction) => {
@@ -24,7 +20,5 @@ app.use((req: Request, res: Response, next: NextFunction) => {
 });
 
 app.use('/', router);
-
 app.use(errorHandler);
-
 app.listen(PORT);
